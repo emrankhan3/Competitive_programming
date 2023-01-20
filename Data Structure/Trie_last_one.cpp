6fb15f4e8cea39b5a634a0d000b19390a479fea6
Emran Khan <63871014+xemran@users.noreply.github.com>
2023-01-20 21:45:12 +0600

    Create Trie_last_one.cpp

---
+++ Data Structure/Trie_last_one.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define wt getchar();
+#define ttt cout<<"T"<<endl;
+#define dbg1(x) cout<<#x<<": "<<x<<endl;
+#define dbg2(x,y) cout<<#x<<": "<<x<<", "<<#y<<": "<<y<<endl;
+#define dbg3(x,y,z) cout<<#x<<": "<<x<<", "<<#y<<": "<<y<<", "<<#z<<": "<<z<<endl;
+#define dbg4(x,y,z,xx) cout<<#x<<": "<<x<<", "<<#y<<": "<<y<<", "<<#z<<", "<<z<<", "<<#xx<<": "<<xx<<endl;
+#define visar(x) for(int i:x) cout<<i<<" "; cout<<endl;
+#define pb push_back
+#define ts cout<<"TTTT"<<endl;
+#define ty int t; cin >> t; while(t--)
+
+// const int mod = 998244353, MAX = 3000005, INF = 1 << 30;
+
+
+
+const int mod  = 1e9 + 7;
+const int eps  = 1e-9 ;
+const int maxn = 4e5 + 5;
+const int inf  = 5e18 ;
+const int minf = -inf ;
+
+
+class Node
+{
+public:
+    Node *ar[27];
+    bool flag = false;
+
+    bool isAvail(char ch) {
+        if (ar[ch - 'a'] == NULL)return false;
+        return true;
+    }
+    void put(char ch, Node *node) {
+        ar[ch - 'a'] = node;
+    }
+    Node *nextch(char ch) {
+        return ar[ch - 'a'];
+    }
+    void setEnd() {
+        flag = true;
+    }
+    bool isEnd() {
+        return flag;
+    }
+};
+
+
+class Trie {
+private:
+    Node *root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    void ins(string word) {
+        Node *node = root;
+        for (char ch : word) {
+            if (!node->isAvail(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->nextch(ch);
+        }
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node *node = root;
+        for (char ch : word) {
+            if (!node->isAvail(ch)) {
+                return false;
+            }
+            node = node->nextch(ch);
+        }
+        return node->isEnd();
+    }
+
+};
+
+
+int32_t main()
+{
+
+#ifdef offCode
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+
+    ios::sync_with_stdio ( 0 );
+    cin.tie ( 0 );
+    cout.tie ( 0 );
+
+    Trie tr;
+
+    tr.ins("apple");
+    tr.ins("append");
+
+
+    if (tr.search("append")) {
+        cout << "paisi";
+    }
+    else cout << "nai";
+
+}
